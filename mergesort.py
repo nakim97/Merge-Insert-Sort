7e04bf096e522a914c165c3220350a57be85c1ca
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-01-18 15:37:39 -0500

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -2,7 +2,7 @@
 # Date: 01/7/2021
 # Assignment: CS 325 HW 1
 
-# Citation for merge sort :  https://www.geeksforgeeks.org/merge-sort/
+# Reference for merge sort :  https://www.geeksforgeeks.org/merge-sort/
 
 def merge_sort(array_list):
     """ this function implements a merge sort to sort an array/vector of integers in ascending order"""
